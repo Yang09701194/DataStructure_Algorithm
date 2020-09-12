615203b588c444d059693e642b7a59941eed64a5
Yang09701194 <mustnecessity@gmail.com>
2020-09-12 09:53:05 +0800

    Queue

---
+++ DSAlgo/DSAlgo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace DSAlgo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Queue q = new Queue();
+			q.Enqueue("test");
+			q.Enqueue(DateTime.Now);
+
+
+			Queue<string> q2 = new Queue<string>();
+			q2.Enqueue("test");
+			//q2.Enqueue(DateTime.Now);
+
+
+
+			Console.Read(); 
+
+		}
+	}
+}
