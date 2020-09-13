e6cd325e5d9fb338b34c2f830575ad123ace1910
Yang09701194 <mustnecessity@gmail.com>
2020-09-13 15:28:08 +0800

    Queue OK

--- DSAlgo/DSAlgo/Program.cs
+++ DSAlgo/DSAlgo/Program.cs
@@ -10,18 +10,7 @@ namespace DSAlgo
 	{
 		static void Main(string[] args)
 		{
-			Queue q = new Queue();
-			q.Enqueue("test");
-			q.Enqueue(DateTime.Now);
-
-
-			Queue<string> q2 = new Queue<string>();
-			q2.Enqueue("test");
-			//q2.Enqueue(DateTime.Now);
-
-
-
-			Console.Read(); 
+			
 
 		}
 	}
---
+++ DSAlgo/DSAlgo/QueueTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSAlgo
+{
+	class QueueTest
+	{
+		public static void Test()
+		{
+			Queue q = new Queue();
+			q.Enqueue("test");
+			q.Enqueue(DateTime.Now);
+
+
+			Queue<string> q2 = new Queue<string>();
+			q2.Enqueue("test");
+			//q2.Enqueue(DateTime.Now);
+
+			q.Clear();
+
+			for (int i = 0; i < 10; i++)
+			{
+				q.Enqueue(i);
+			}
+
+			q.Dequeue();
+			q.Dequeue();
+
+			foreach (object obj in q)
+			{
+				Console.WriteLine(obj);
+			}
+
+			Console.WriteLine(q.Contains(3));
+			Console.WriteLine(q.Count);
+
+			Console.Read();
+
+
+		}
+
+	}
+}
