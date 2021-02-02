6fc72a64fca0b248cca3a147a9fb61a677ceafc0
Yang09701194 <mustnecessity@gmail.com>
2021-02-03 07:58:26 +0800

    20210203

---
+++ DSAlgo/DSAlgo/DS/TestList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSAlgo.DS
+{
+	class TestList
+	{
+
+		//https://social.msdn.microsoft.com/Forums/en-US/73480a1e-a999-4509-943a-8dfa3227a5dd/complexity-of-list-operations?forum=csharpgeneral&prof=required
+		// Question
+		// You cannot vote
+		// 2
+		//
+		// Add O(1)   [note: the cost of array doubling is "amortized" and does not affect this]
+		// 
+		// Remove O(N)
+		// 
+		// RemoveAt O(N)  [must shift elements at higher indices]
+		// 
+		// Item[] O(1)
+		// 
+		// Count O(1)
+
+	}
+
+}
